a67c2b638fe7d4f6b45cf54416375a641c0aafe1
taposh99 <taposhmd2@gmail.com>
2023-12-13 01:04:21 +0600

    api get data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Task;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Database\QueryException;
+
 
 
 class HomeController extends Controller
@@ -21,19 +23,19 @@ class HomeController extends Controller
             'homes' => $homes, // Pass the 'students' variable to the view
         ]);
     }
-//    api  get data
+    //    api  get data
     public function test()
     {
         try {
             $tasks = Task::latest()->get();
-            
+
             return $tasks;
         } catch (\Exception $e) {
             // Log the exception or handle it in a way that makes sense for your application
             return response()->json(['error' => 'Error fetching tasks'], 500);
         }
     }
-    
+
 
 
 
@@ -48,7 +50,7 @@ class HomeController extends Controller
 
         ]);
 
-        
+
 
         Task::create([
             'p_id' => $request->p_id,
@@ -61,6 +63,32 @@ class HomeController extends Controller
         return back()->with('success', 'Data created successfully');
     }
 
+    public function apistore(Request $request)
+    {
+        try {
+            $request->validate([
+                'p_id' => 'required',
+                'tittle' => 'required',
+                'description' => 'required',
+                'complete' => 'required',
+            ]);
+
+            Task::create([
+                'p_id' => $request->p_id,
+                'tittle' => $request->tittle,
+                'description' => $request->description,
+                'complete' => $request->complete,
+            ]);
+
+            return response()->json(['success' => 'Data created successfully'], 200);
+        } catch (QueryException $e) {
+            // Log the exception or handle it in a way that makes sense for your application
+            return response()->json(['error' => 'Database error: ' . $e->getMessage()], 500);
+        } catch (\Exception $e) {
+            // Log the exception or handle it in a way that makes sense for your application
+            return response()->json(['error' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
     public function edit($id)
     {
@@ -88,7 +116,7 @@ class HomeController extends Controller
 
 
     public function destroy(Request $request)
-    
+
     {
         Task::destroy($request->data_id);
 
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/home', [HomeController::class, 'test']);
+Route::post('store', [HomeController::class, 'apistore'])->name('store');
+
